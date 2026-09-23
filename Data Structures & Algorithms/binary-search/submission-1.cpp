2fdd8b6ab7bf474e62e71a0d7658226daf28cbaa
roberto acosta <robacomail@protonmail.com>
2026-09-23 16:34:14 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l <= r) {
+            int midpoint = l + (r - l)/2;
+
+            if(target > nums[midpoint]) {
+                l = midpoint + 1;
+            }
+            else if (target < nums[midpoint]) {
+                r = midpoint - 1;
+            }   
+            else {
+                return midpoint;
+            }
+
+        }
+        return -1;
+    }
+};
